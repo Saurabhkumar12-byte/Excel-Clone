49c4dbc9ebe903c729714d3075cc41d16b8c2929
Saurabh <saurabhsingh4778698@gmail.com>
2021-12-01 22:50:45 +0530

    basic fixes

--- Script.js
+++ Script.js
@@ -1,6 +1,7 @@
 let leftDiv = document.querySelector(".left");
 let rightDiv= document.querySelector(".right-top");
 let rightBottomDiv= document.querySelector(".right-bottom");
+let addressBar = document.querySelector("#address-bar");
 for (let i = 1; i < 101; i++) {
     let div=document.createElement("div");
     div.setAttribute("class","cell");
@@ -20,9 +21,21 @@ for (let i = 1; i < 101; i++) {
    for (let j = 65; j <91; j++) {
     let div=document.createElement("div");
     div.setAttribute("class","right-main-cell");
-    div.setAttribute("contenteditable", "true")
+    div.setAttribute("contenteditable", "true");
+    getCellAdd(div,i,j);
     
     rightBottomDiv.appendChild(div);
        
    }
+}
+
+function getCellAdd(div, i ,j){
+    let cell= div;
+    cell.addEventListener("click",()=>{
+        console.log(i);
+        console.log(String.fromCharCode(j));
+        addressBar.value=` ${String.fromCharCode(j)}${i}`;
+        
+    })
+
 }
